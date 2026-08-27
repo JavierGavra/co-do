cd2daf84ba949c5b2cab978799214b70d2f9cad8
Lenovo Gavra <gavrajavier01@gmail.com>
2026-08-27 23:11:55 +0700

    feat: Add CI/CD for development and staging

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -52,11 +52,14 @@ android {
     }
 
     buildTypes {
-        release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            // signingConfig = signingConfigs.getByName("debug")
+        getByName("debug") {
+        }
 
+        create("staging") {
+            initWith(getByName("debug"))
+        }
+        
+        release {
             signingConfig = signingConfigs.getByName("release")
             isMinifyEnabled = true
             isShrinkResources = true
